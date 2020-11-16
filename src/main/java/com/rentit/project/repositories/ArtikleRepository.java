ea8f882828e8828da0f04bd906d702dda696a11e
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2020-11-16 20:58:24 +0100

    Update ArtikleRepository.java

--- src/main/java/com/rentit/project/repositories/ArtikleRepository.java
+++ src/main/java/com/rentit/project/repositories/ArtikleRepository.java
@@ -3,9 +3,9 @@ package com.rentit.project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rentit.project.models.Artikle;
+import com.rentit.project.models.Artikel;
 
 @Repository
-public interface ArtikleRepository extends JpaRepository<Artikle, Long> {
+public interface ArtikleRepository extends JpaRepository<Artikel, Long> {
 
 }
