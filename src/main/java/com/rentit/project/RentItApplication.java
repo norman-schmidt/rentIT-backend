2e47546eb6082ef336e970554ac96ed62aa797b8
baudoin keunne <keunne.baudoin@yahoo.fr>
2020-12-13 17:35:46 +0100

    add command line runner for category

--- src/main/java/com/rentit/project/RentItApplication.java
+++ src/main/java/com/rentit/project/RentItApplication.java
@@ -1,16 +1,33 @@
 package com.rentit.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.rentit.project.models.CategoryEntity;
+import com.rentit.project.repositories.CategoryRepository;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class RentItApplication {
+public class RentItApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentItApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		CategoryEntity cat = new CategoryEntity(1, "Phones", null);
+		
+		categoryRepository.save(cat);
+		
 	}
 
 }
