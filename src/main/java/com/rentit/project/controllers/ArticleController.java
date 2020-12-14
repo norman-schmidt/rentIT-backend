0f7e1819abc76303ec5cd12618d974a0f689c474
baudoin keunne <keunne.baudoin@yahoo.fr>
2020-12-14 18:51:09 +0100

    add cross origin on all controllers

--- src/main/java/com/rentit/project/controllers/ArticleController.java
+++ src/main/java/com/rentit/project/controllers/ArticleController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.rentit.project.services.RentalService;
 
 @RestController
 @RequestMapping("/api/articles/")
+@CrossOrigin(origins = "*")
 public class ArticleController {
 
 	@Autowired
--- src/main/java/com/rentit/project/controllers/ArticleQuantityController.java
+++ src/main/java/com/rentit/project/controllers/ArticleQuantityController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.rentit.project.services.RentalService;
 
 @RestController
 @RequestMapping("/api/quantities")
+@CrossOrigin(origins = "*")
 public class ArticleQuantityController {
 
 	@Autowired
