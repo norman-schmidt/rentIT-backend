6bef06b9f7b4bccb848fed0a6e1a91a1f1f3b772
baudouin <keunne.baudoin@yahoo.fr>
2021-01-16 16:56:36 +0100

    remove security on swagger

--- src/main/java/com/rentit/project/config/WebSecurityConfig.java
+++ src/main/java/com/rentit/project/config/WebSecurityConfig.java
@@ -57,6 +57,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+			.antMatchers("/api/categories/**").permitAll()
+			.antMatchers("/api/articles/**").permitAll()
+			.antMatchers("/swagger-ui.html#/**").permitAll()
 			.antMatchers(HttpMethod.POST).permitAll()
 			.anyRequest().authenticated();
 
