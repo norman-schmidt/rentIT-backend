7fb62fe702393525a6b7a14e51b81b9e4b4cb4b6
baudouin <keunne.baudoin@yahoo.fr>
2021-02-02 15:36:11 +0100

    remove request header on sigin

--- src/main/java/com/rentit/project/controllers/AuthController.java
+++ src/main/java/com/rentit/project/controllers/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +31,6 @@ import com.rentit.project.pojo.response.MessageResponse;
 import com.rentit.project.repositories.RoleRepository;
 import com.rentit.project.repositories.UserRepository;
 import com.rentit.project.services.UserDetailsImpl;
-import com.rentit.project.services.UserService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -53,22 +51,8 @@ public class AuthController {
 	@Autowired
 	JwtUtils jwtUtils;
 
-	@Autowired
-	private UserService userService;
-
 	@PostMapping("/signin")
-	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,
-			@RequestHeader(value = "Authorization") String authHeader) {
-
-		String token = authHeader.substring(7, authHeader.length());
-		String username = null;
-		if (jwtUtils.validateJwtToken(token)) {
-			username = jwtUtils.getUserNameFromJwtToken(token);
-		}
-
-		UserEntity user = userService.findUserByEmail(username);
-
-		System.out.println(user);
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
 		Authentication authentication = authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
