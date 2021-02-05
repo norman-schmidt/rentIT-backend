0edae62a011c407faa88ae037a862bfa3dc6856a
baudouin <keunne.baudoin@yahoo.fr>
2021-02-06 00:21:54 +0100

    customize JWT Response

--- src/main/java/com/rentit/project/pojo/response/JwtResponse.java
+++ src/main/java/com/rentit/project/pojo/response/JwtResponse.java
@@ -1,6 +1,10 @@
 package com.rentit.project.pojo.response;
 
+import java.util.Date;
 import java.util.List;
+
+import com.rentit.project.models.ImageEntity;
+import com.rentit.project.models.RentalEntity;
 
 import lombok.Data;
 
@@ -15,14 +19,31 @@ public class JwtResponse {
 	private String lastname;
 	private String firstname;
 
-	public JwtResponse(String accessToken, long id, String email, String lastname, String firstname,
-			List<String> roles) {
+	private String street;
+	private String hausNumber;
+	private int plz;
+	private String ort;
+	private Date birthday;
+	private List<RentalEntity> rental;
+	private ImageEntity image;
+
+	public JwtResponse(String accessToken, long id, String email, String lastname, String firstname, List<String> roles,
+			String street, String hausNumber, int plz, String ort, Date birthday, List<RentalEntity> rental,
+			ImageEntity image) {
 		this.token = accessToken;
 		this.id = id;
 		this.email = email;
 		this.roles = roles;
 		this.lastname = lastname;
 		this.firstname = firstname;
+
+		this.street = street;
+		this.hausNumber = hausNumber;
+		this.plz = plz;
+		this.ort = ort;
+		this.birthday = birthday;
+		this.rental = rental;
+		this.image = image;
 	}
 
 }
