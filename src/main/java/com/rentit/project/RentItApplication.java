ee9a18d375aac25741c44242f2e64d2220f6f8c5
baudouin <keunne.baudoin@yahoo.fr>
2021-02-05 16:48:27 +0100

    remove commandline runner on start

--- src/main/java/com/rentit/project/RentItApplication.java
+++ src/main/java/com/rentit/project/RentItApplication.java
@@ -1,48 +1,42 @@
 package com.rentit.project;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.rentit.project.models.ERole;
-import com.rentit.project.models.Role;
-import com.rentit.project.repositories.RoleRepository;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
-public class RentItApplication implements CommandLineRunner {
+public class RentItApplication {
 
 // public class RentItApplication implements CommandLineRunner { 
 
-	@Autowired
-	private RoleRepository roleRepository;
+//	@Autowired
+//	private RoleRepository roleRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentItApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Role role1 = new Role();
-		role1.setId(1);
-		role1.setName(ERole.ROLE_ADMIN);
-		roleRepository.save(role1);
-
-		Role role2 = new Role();
-		role2.setId(2);
-		role2.setName(ERole.ROLE_MODERATOR);
-		roleRepository.save(role2);
-
-		Role role3 = new Role();
-		role3.setId(3);
-		role3.setName(ERole.ROLE_USER);
-		roleRepository.save(role3);
-
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Role role1 = new Role();
+//		role1.setId(1);
+//		role1.setName(ERole.ROLE_ADMIN);
+//		roleRepository.save(role1);
+//
+//		Role role2 = new Role();
+//		role2.setId(2);
+//		role2.setName(ERole.ROLE_MODERATOR);
+//		roleRepository.save(role2);
+//
+//		Role role3 = new Role();
+//		role3.setId(3);
+//		role3.setName(ERole.ROLE_USER);
+//		roleRepository.save(role3);
+//
+//	}
 
 }
