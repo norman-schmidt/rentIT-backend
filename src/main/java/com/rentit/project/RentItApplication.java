cb2e0fc5ac10c6f75cf3b8606d29ac8afb4f2671
baudouin <keunne.baudoin@yahoo.fr>
2021-02-05 16:36:17 +0100

    save roles on start

--- src/main/java/com/rentit/project/RentItApplication.java
+++ src/main/java/com/rentit/project/RentItApplication.java
@@ -1,49 +1,48 @@
 package com.rentit.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.rentit.project.models.ERole;
+import com.rentit.project.models.Role;
+import com.rentit.project.repositories.RoleRepository;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
-public class RentItApplication {
-	
-// public class RentItApplication implements CommandLineRunner { -> pour executer les repositories au demarage
+public class RentItApplication implements CommandLineRunner {
 
-//	@Autowired
-//	private ArticleRepository articleRepository;
-//
-//	@Autowired
-//	private ImageRepository imageRepository;
-//
-//	@Autowired
-//	private InvoiceRepository invoiceRepository;
-//	
-//	@Autowired
-//	private UserRepository userRepository;
+// public class RentItApplication implements CommandLineRunner { 
+
+	@Autowired
+	private RoleRepository roleRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentItApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//		ArticleEntity art = new ArticleEntity(1, "IPhone x", "DEG542DF", "v12", 12, 900, "Description", null , null, null, null);
-//		articleRepository.save(art);
-//
-//		ImageEntity img = new ImageEntity(1, "image link 1", "image type", null, null);
-//		imageRepository.save(img);
-//
-//		Date date = new Date();
-//		InvoiceEntity invoice = new InvoiceEntity(1, 2, date, null);
-//		invoiceRepository.save(invoice);
-//		
-//		UserEntity user = new UserEntity(1, "test@test.com", "testPassword", "Jack", "Franck", "Magdeburger Straße 10", date, null, null);
-//		userRepository.save(user);
-//		
-//	}
+	@Override
+	public void run(String... args) throws Exception {
+		Role role1 = new Role();
+		role1.setId(1);
+		role1.setName(ERole.ROLE_ADMIN);
+		roleRepository.save(role1);
+
+		Role role2 = new Role();
+		role2.setId(2);
+		role2.setName(ERole.ROLE_ADMIN);
+		roleRepository.save(role2);
+
+		Role role3 = new Role();
+		role3.setId(3);
+		role3.setName(ERole.ROLE_ADMIN);
+		roleRepository.save(role3);
+
+	}
 
 }
