4485be2d502e9abf6faf4bcdfc28c256e19a8d3c
BorisDvn <dovonon@th-brandenburg.de>
2021-02-19 10:53:23 +0100

    @PatchMapping to Category
    we can update name and description

--- src/main/java/com/rentit/project/controllers/CategoryController.java
+++ src/main/java/com/rentit/project/controllers/CategoryController.java
@@ -2,12 +2,20 @@ package com.rentit.project.controllers;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -74,6 +82,37 @@ public class CategoryController {
 	@PutMapping("{id_category}/article/{id_article}/remove")
 	public CategoryEntity removeCategoryImage(@PathVariable long id_category, @PathVariable long id_article) {
 		return categoryService.removeCategoryImage(id_category, id_article);
+	}
+
+	@PatchMapping("{id}")
+	public ResponseEntity<MessageResponse> updatePerson(@Valid @PathVariable("id") long id,
+			@RequestBody Map<String, Object> categoryEntity) {
+
+		CategoryEntity _categoryEntity = categoryService.getCategory(id);
+
+		categoryEntity.forEach((element, value) -> {
+			switch (element) {
+			case "name":
+				_categoryEntity.setName((String) value);
+				// break;
+			case "description":
+				_categoryEntity.setDescription((String) value);
+				break;
+			}
+		});
+
+		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+		Set<ConstraintViolation<CategoryEntity>> violations = validator.validate(_categoryEntity);// , OnUpdate.class);
+
+		if (!violations.isEmpty()) {
+			// When invalid
+			return ResponseEntity.badRequest().body(new MessageResponse(violations.toString()));
+		}
+
+		categoryService.addCategory(_categoryEntity);
+
+		return ResponseEntity.ok().body(new MessageResponse("Successfully updated"));
 	}
 
 }
